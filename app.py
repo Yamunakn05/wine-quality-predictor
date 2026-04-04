adde16b9378bbbbe0c0fc069f81540f3fd9d5c85
Yamunakn05 <yamunakn50@gmail.com>
2026-04-04 21:57:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,12 +50,13 @@ model_option = st.sidebar.selectbox(
 threshold = st.sidebar.slider("Prediction Threshold", 0.1, 0.9, 0.4)
 
 if model_option == "Logistic Regression":
-    model = LogisticRegression(class_weight='balanced')
-elif model_option == "KNN":
-    model = KNeighborsClassifier(n_neighbors=7)
-else:
-    model = RandomForestClassifier(n_estimators=200, class_weight='balanced')
+    model = LogisticRegression(class_weight='balanced', max_iter=1000)
 
+elif model_option == "KNN":
+    model = KNeighborsClassifier( n_neighbors=9,weights='distance' )
+
+else:
+    model = RandomForestClassifier(n_estimators=300,max_depth=12,min_samples_split=5,class_weight='balanced',random_state=42  )
 model.fit(X_train, y_train)
 
 st.subheader("🧠 Model Performance Analysis")
@@ -71,26 +72,27 @@ st.subheader("🧪 Input Wine Characteristics")
 
 input_data = {}
 for col in features:
-    input_data[col] = st.slider(
-        col,
-        float(df[col].min()),
-        float(df[col].max())
-    )
+    input_data[col] = st.slider(col,float(df[col].min()),float(df[col].max()) )
 
 input_df = pd.DataFrame([input_data])
 input_scaled = scaler.transform(input_df)
 
 st.subheader("🔮 Prediction Outcome")
 
-if st.button("Predict Quality"):
+if st.button("Predict"):
     prob = model.predict_proba(input_scaled)[0][1]
-
-    st.write(f"Prediction Confidence: {prob:.2f}")
-
-    if prob > threshold:
-        st.success("✅ High Quality Wine Detected")
+    st.write(f"Prediction Probability: {prob:.2f}")
+    if model_option == "KNN":
+        threshold_used = 0.35
+    elif model_option == "Random Forest":
+        threshold_used = 0.4
     else:
-        st.error("❌ Low Quality Wine Detected")
+        threshold_used = threshold
+
+    if prob > threshold_used:
+        st.success("✅ Good Quality Wine")
+    else:
+        st.error("❌ Bad Quality Wine")
 
 st.subheader("🔗 Feature Correlation Insights")
 
